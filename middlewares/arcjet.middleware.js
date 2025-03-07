37eb4a746743f424150cd00c50418e16fdb696f1
Aarya Chopkar <aaryachopkar@gmail.com>
2025-03-07 21:35:00 +0530

    feat: arcjet installed

---
+++ middlewares/arcjet.middleware.js
@@ -0,0 +1,34 @@
+import aj from "../config/arcjet.js";
+
+const arcjetMiddleware = async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, { requested: 1 });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return res.status(429).json({
+          error: "Rate limit exceeded",
+          details: decision.reason,
+        });
+      }
+
+      if (decision.reason.isBot()) {
+        return res.status(403).json({
+          error: "Bot detected",
+          details: decision.reason,
+        });
+      }
+
+      return res
+        .status(403)
+        .json({ error: "Access denied", details: decision.reason });
+    }
+
+    next();
+  } catch (error) {
+    console.log(`Arcjet Error - ${error}`);
+    next(error);
+  }
+};
+
+export default arcjetMiddleware;
