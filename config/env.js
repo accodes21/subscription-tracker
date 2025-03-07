7333a31eedde6e36d1649788664edc13478bb308
Aarya Chopkar <aaryachopkar@gmail.com>
2025-03-07 21:35:46 +0530

    feat: add arcjet middleware and environment variables

--- config/env.js
+++ config/env.js
@@ -3,5 +3,12 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, NODE_ENV, DATABASE_URL, JWT_SECRET, JWT_EXPIRES_IN } =
-  process.env;
+export const {
+  PORT,
+  NODE_ENV,
+  DATABASE_URL,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  ARCJET_ENV,
+  ARCJET_API_KEY,
+} = process.env;
